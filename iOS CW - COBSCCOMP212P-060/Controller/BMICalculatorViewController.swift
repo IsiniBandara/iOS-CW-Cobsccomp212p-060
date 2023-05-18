f97126ebe30ddf0fc4476a896c50f397fdc5cb60
IsiniBandara <67850663+IsiniBandara@users.noreply.github.com>
2023-05-18 22:14:34 +0530

    Develop: BMI Calculator and Age Selector

---
+++ iOS CW - COBSCCOMP212P-060/Controller/BMICalculatorViewController.swift
@@ -0,0 +1,97 @@
+//
+//  BMICalculatorViewController.swift
+//  iOS CW - COBSCCOMP212P-060
+//
+//  Created by Isini Bandara on 2023-05-18.
+//
+
+import UIKit
+
+class BMICalculatorViewController: UIViewController {
+    private let weightTextField = UITextField()
+    private let heightTextField = UITextField()
+    private let calculateButton = UIButton(type: .system)
+    private let resultLabel = UILabel()
+    private let fitnessPlanLabel = UILabel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureUI()
+        addConstraints()
+    }
+
+    private func configureUI() {
+        view.backgroundColor = .white
+
+        weightTextField.placeholder = "Weight (kg)"
+        weightTextField.borderStyle = .roundedRect
+        weightTextField.keyboardType = .decimalPad
+
+        heightTextField.placeholder = "Height (cm)"
+        heightTextField.borderStyle = .roundedRect
+        heightTextField.keyboardType = .decimalPad
+
+        calculateButton.setTitle("Calculate", for: .normal)
+        calculateButton.addTarget(self, action: #selector(calculateButtonTapped), for: .touchUpInside)
+
+        resultLabel.textAlignment = .center
+        fitnessPlanLabel.textAlignment = .center
+
+        [weightTextField, heightTextField, calculateButton, resultLabel, fitnessPlanLabel].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview($0)
+        }
+    }
+
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            weightTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            weightTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            weightTextField.widthAnchor.constraint(equalToConstant: 200),
+            
+            heightTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            heightTextField.topAnchor.constraint(equalTo: weightTextField.bottomAnchor, constant: 20),
+            heightTextField.widthAnchor.constraint(equalToConstant: 200),
+            
+            calculateButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            calculateButton.topAnchor.constraint(equalTo: heightTextField.bottomAnchor, constant: 40),
+            
+            resultLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            resultLabel.topAnchor.constraint(equalTo: calculateButton.bottomAnchor, constant: 40),
+            
+            fitnessPlanLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            fitnessPlanLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            fitnessPlanLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            fitnessPlanLabel.topAnchor.constraint(equalTo: resultLabel.bottomAnchor, constant: 20)
+        ])
+    }
+
+    @objc private func calculateButtonTapped() {
+        guard let weightText = weightTextField.text, let weight = Double(weightText),
+              let heightText = heightTextField.text, let height = Double(heightText) else {
+            resultLabel.text = "Invalid input"
+            return
+        }
+
+        let heightInMeter = height / 100.0
+        let bmi = weight / (heightInMeter * heightInMeter)
+        resultLabel.text = "BMI: \(String(format: "%.2f", bmi))"
+
+        let fitnessPlan = suggestFitnessPlan(bmi: bmi)
+        fitnessPlanLabel.text = fitnessPlan
+    }
+
+    private func suggestFitnessPlan(bmi: Double) -> String {
+        switch bmi {
+        case ..<18.5:
+            return "Underweight: Consider gaining some weight through a balanced diet and strength training."
+        case 18.5..<25.0:
+            return "Normal weight: Maintain a healthy lifestyle with regular exercise and a balanced diet."
+        case 25.0..<30.0:
+            return "Overweight: Focus on weight loss through portion control, regular exercise, and a balanced diet."
+        default:
+            return "Obese: Consult a healthcare professional for a personalized fitness and nutrition plan."
+        }
+    }
+}
--- iOS CW - COBSCCOMP212P-060/Controller/SelectAgeViewController.swift
+++ iOS CW - COBSCCOMP212P-060/Controller/SelectAgeViewController.swift
@@ -7,23 +7,96 @@
 
 import UIKit
 
-class SelectAgeViewController: UIViewController {
+class SelectAgeViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    private var pickerView: UIPickerView!
+    private let nextButton = CustomButton(title: "Next", hasBackground: true, fontSize: .big)
+    private let headingTextView = CustomTextView(title: "How old are you ?", fontSize: .big)
+    private let subheadingTextView = CustomTextView(title: "This helps us create your personalized plan", fontSize: .small)
+    private var options: [String] = ["18", "19", "20","21", "22", "23","24", "25", "26","27", "28", "29","30", "31", "32","33", "34", "35","36", "37", "38", "39", "40", "41", "42", "43", "44","45", "46", "47", "48", "49", "50"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor(named: "Black")
+        setupUI()
+        self.nextButton.addTarget(self, action: #selector(didTapNext), for: .touchUpInside)
     }
+
+func setupUI(){
+    pickerView = UIPickerView(frame: CGRect(x: 0, y: 200, width: view.frame.width, height: 400))
+    pickerView.dataSource = self
+    pickerView.delegate = self
     
+    headingTextView.translatesAutoresizingMaskIntoConstraints = false
+    subheadingTextView.translatesAutoresizingMaskIntoConstraints = false
+    nextButton.translatesAutoresizingMaskIntoConstraints = false
 
-    /*
-    // MARK: - Navigation
+    view.addSubview(headingTextView)
+    view.addSubview(subheadingTextView)
+    view.addSubview(nextButton)
+    view.addSubview(pickerView)
+    
+    NSLayoutConstraint.activate([
+        self.headingTextView.topAnchor.constraint(equalTo: self.view.layoutMarginsGuide.topAnchor),
+        self.headingTextView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+        self.headingTextView.heightAnchor.constraint(equalToConstant: 50),
+        self.headingTextView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1),
+        
+        self.subheadingTextView.topAnchor.constraint(equalTo: self.headingTextView.bottomAnchor, constant: 10),
+        self.subheadingTextView.centerXAnchor.constraint(equalTo: self.headingTextView.centerXAnchor),
+        self.subheadingTextView.heightAnchor.constraint(equalToConstant: 100),
+        self.subheadingTextView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        self.nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        self.nextButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -80.0),
+        self.nextButton.heightAnchor.constraint(equalToConstant: 55),
+        self.nextButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.50),
+    ])
+
+}
+    @objc func didTapNext() {
+
     }
-    */
+
+    // MARK: - UIPickerViewDataSource
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1 // Number of columns in the picker view
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return options.count // Number of rows in the picker view
+    }
+
+    // MARK: - UIPickerViewDelegate
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return options[row] // Title for each row in the picker view
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedOption = options[row] // Handle the selected option
+        print("Selected option: \(selectedOption)")
+    }
+
+//        func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+//            let title = options[row]
+//            let attributes: [NSAttributedString.Key: Any] = [
+//                .foregroundColor: UIColor.white, // Customize the text color here
+//                .font: UIFont.systemFont(ofSize: 30, weight: .bold)
+//            ]
+//
+//            return NSAttributedString(string: title, attributes: attributes)
+//        }
+func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+    let label = UILabel()
+    label.textAlignment = .center
+    label.textColor = UIColor(named: "Green")
+    label.font = UIFont.systemFont(ofSize: 30) // Adjust the font size as desired
+    let selectedOption = options[row]
+    label.text = selectedOption// Provide the text for the row
+    
+    return label
+}
+    
 
 }
